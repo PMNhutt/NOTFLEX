0a652e8ea62d6afcae5754187421a5f1781333ce
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2022-03-11 14:56:16 +0700

    update
    remove animation from navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import './Navbar.css';
 import React, { useEffect, useState } from 'react';
-import { motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { Link, NavLink } from "react-router-dom";
 import logo from '../../logo/netflix_acc.png';
 import SearchIcon from '@mui/icons-material/Search';
@@ -28,22 +28,22 @@ function Navbar() {
 
   return (
     <motion.div className={`${scroll ? "navbar_black" : "navbar"}`}
-      initial = {{y: -200}}
-      animate = {{y : 0}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
     >
 
       <ul className="navbar-left">
-        <li style={{lineHeight: 0}}><Link to="/home"><img className="navbar-logo" src="https://fontmeme.com/permalink/220212/2b79b3189fcc6673d3153dee728478eb.png" alt="netflix-font" border="0" /></Link></li>
+        <li style={{ lineHeight: 0 }}><Link to="/home"><img className="navbar-logo" src="https://fontmeme.com/permalink/220212/2b79b3189fcc6673d3153dee728478eb.png" alt="netflix-font" border="0" /></Link></li>
         <li className="navbar-link "><NavLink to="/home" className={(navData) => navData.isActive ? "active" : "nav-child"}>Home</NavLink></li>
         <li className="navbar-link " ><NavLink to="/tvshows" className={(navData) => navData.isActive ? "active" : "nav-child"} >TV Shows</NavLink></li>
         <li className="navbar-link"><NavLink to="/movies" className={(navData) => navData.isActive ? "active" : "nav-child"}>Movies</NavLink></li>
       </ul>
 
       <div className="navbar-right">
-        <SearchIcon sx={{marginRight: '1.3vw', fontSize: '2vw', cursor: 'pointer'}}/>
-        <NotificationsIcon sx={{marginRight: '1.3vw', fontSize: '2vw', cursor: 'pointer'}}/>
+        <SearchIcon sx={{ marginRight: '1.3vw', fontSize: '2vw', cursor: 'pointer' }} />
+        <NotificationsIcon sx={{ marginRight: '1.3vw', fontSize: '2vw', cursor: 'pointer' }} />
         <Link to="/home"><img className="navbar-acc" src={logo} alt="account_logo" border="0" /></Link>
-        <ArrowDropDownIcon sx={{cursor: 'pointer'}}/>
+        <ArrowDropDownIcon sx={{ cursor: 'pointer' }} />
       </div>
     </motion.div>
 
